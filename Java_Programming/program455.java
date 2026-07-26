e6c2b80d7adc17a5d303777a1a16bec44fd27fc8
Abhijeet Gorale <abhigorale99@gmail.com>
2026-07-26 23:33:13 +0530

    Add files via upload

---
+++ Java_Programming/program455.java
@@ -0,0 +1,121 @@
+class program455
+{
+    public static void main(String[] A)
+    {
+        SinglyLL sobj = new SinglyLL();
+        int iRet = 0 ;
+        
+        sobj.InsertFirst(51);
+        sobj.InsertFirst(21);
+        sobj.InsertFirst(11);
+
+        sobj.InsertLast(101);
+        sobj.InsertLast(111);
+        sobj.InsertLast(121);
+
+        sobj.Display();
+
+        iRet  = sobj.Count();
+        System.out.println("Number of node are  :"+iRet);
+
+    }
+}
+
+class SinglyLL
+{
+    private node first ;
+    private int iCount ;
+
+    public SinglyLL()
+    {
+        System.out.println("Inside Constructor");
+        this.first  = null ;
+        this.iCount = 0 ;
+
+    }
+
+    public void Display()
+    {
+        node temp = first ;
+
+        while (temp != null)
+        {
+            System.out.print("| "+temp.data+" | -> ");
+            temp = temp.next ;
+
+        }
+        System.out.println("null");
+    }
+
+    public int Count()
+    {
+        return iCount ;
+    }
+
+
+    public void InsertFirst(int ino)
+    {
+        node newn = new node(ino) ;
+
+        if(first == null)
+        {
+            first = newn ;
+        }
+        else
+        {
+            newn.next = first ;
+            first = newn ;
+        }
+        iCount++;
+        
+    }
+
+    public void InsertLast(int ino)
+    {
+        node newn = new node(ino) ;
+
+        if(first == null)
+        {
+            first = newn ;
+        }
+        else
+        {
+            node temp = first ;
+            while(temp.next != null)
+            {
+                temp = temp.next;
+            }
+            temp.next = newn ;
+        }
+        iCount++;
+        
+    }
+
+    public void InsertAtPosition(int ino,int iPos)
+    {}
+
+    public void DeleteFirst()
+    {}
+
+    public void DeleteLast()
+    {}
+
+    public void DeleteAtPos(int iPos)
+    {}
+
+}
+class node
+{
+    public int data ;
+    public node next ;
+
+    node(int no)
+    {
+        this.data = no ;
+        this.next = null ;
+    }
+
+}
+
+
+
