5337d64ac3bb50e3502704b72f5d0c23704c806d
Abhijeet Gorale <abhigorale99@gmail.com>
2026-07-27 23:19:43 +0530

    Add files via upload

---
+++ CPP-Programming/program466.cpp
@@ -0,0 +1,121 @@
+#include<iostream>
+using namespace std ;
+
+#pragma pack(1)
+struct node
+{
+    int data ;
+    struct node *next ;
+
+};
+
+class Stack
+{
+    private :
+        struct node *first ;
+        int iCount ;
+
+
+    public :
+
+        Stack();
+        void push(int iNo) ;    // InsertFirst
+        int pop();          // DeleteFirst
+        int Peep();        // DeleteFirst
+        void Display();
+        int Count();
+
+};
+
+Stack :: Stack()
+{
+    this->first = NULL ;
+    this->iCount = 0 ;
+}
+void Stack :: push(int iNo)
+{
+    struct node *newn = NULL;
+
+    newn = new struct node();
+
+    newn ->data = iNo ;
+    newn -> next = NULL;
+
+    newn -> next = first ;
+    first = newn ;                                          
+                                    
+    iCount++;
+} 
+int Stack :: pop() 
+{
+    int iValue  = 0 ;
+    struct node * temp = NULL ;
+
+   if(first == NULL)
+   {
+    cout<<"Stack is empty\n";
+    return -1 ;
+   }
+   else
+   {
+        iValue = first->data ;
+        temp = first ;
+
+        first = first -> next ;
+        delete temp ;
+
+        iCount-- ;
+        return iValue ;
+        
+   }
+}
+int Stack ::  Peep() 
+{
+    return 0 ;
+}       
+void Stack :: Display()
+{
+    struct node *temp = NULL ;
+
+    temp = first ;
+
+    while(temp!= NULL)
+    {
+        cout<<"| "<<temp->data<<" |\n";
+        temp =temp -> next ;
+    }
+}
+int Stack ::  Count()
+{
+   return iCount ;  
+}
+
+
+
+int main()
+{
+    Stack sobj ;
+    int iRet = 0 ;
+
+    sobj.push(11);
+    sobj.push(21);
+    sobj.push(51);
+    sobj.push(101);
+
+    sobj.Display();
+    iRet = sobj.Count();
+
+    cout<<"Elements of the Stack are :"<<iRet<<"\n";
+
+    iRet = sobj.pop();
+
+    cout<<"popped element is :"<<iRet<<"\n";
+
+    sobj.Display();
+    iRet = sobj.Count();
+
+    cout<<"Elements of the Stack are :"<<iRet<<"\n";
+    
+
+    return 0 ;  
+}
