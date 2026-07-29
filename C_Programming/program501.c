71a7030fd7d49e27a902ab1bcb098eb18a2237a8
Abhijeet Gorale <abhigorale99@gmail.com>
2026-07-29 20:08:11 +0530

    Add files via upload

---
+++ C_Programming/program501.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void Display()
+{
+    auto int i = 0 ;
+
+    i = 1 ;
+    while(i<=4)
+    {
+        printf("Jay Ganeshh...\n");
+        i++;
+    }
+    
+}
+
+int main()
+{
+    Display();
+
+    return 0 ;
+}
