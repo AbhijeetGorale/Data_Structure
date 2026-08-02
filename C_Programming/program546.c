2230b4299fd960a29464db3d0b4ae035d3eb5164
Abhijeet Gorale <abhigorale99@gmail.com>
2026-08-02 23:52:35 +0530

    Add files via upload

---
+++ C_Programming/program546.c
@@ -0,0 +1,34 @@
+// 
+// 
+
+#include<stdio.h>
+
+int strlenX(char *str)
+{
+    static int iCount = 0 ;
+
+    if(*str != '\0')
+    {
+        iCount++;
+        str++;
+
+        strlenX(str);
+    }
+    return iCount ;
+}
+
+int main()
+{
+    int iRet= 0 ;
+    char Arr[20] = {'\0'};
+
+    printf("Enter String :\n");
+    scanf("%[^'\n']s",Arr);
+
+    iRet = strlenX(Arr);
+
+    printf("length of Arr is :%d\n",iRet);
+
+
+    return 0 ;
+}
