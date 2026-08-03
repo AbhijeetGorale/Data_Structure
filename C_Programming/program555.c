3fe34d1c76b7b1deb690253bef3e6fa938cb1721
Abhijeet Gorale <abhigorale99@gmail.com>
2026-08-03 20:10:33 +0530

    Add files via upload

---
+++ C_Programming/program555.c
@@ -0,0 +1,84 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *lchild;
+    struct node *rchild;
+};
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node **PPNODE;
+
+void Insert(PPNODE first, int iNo)
+{
+    PNODE temp = NULL ;
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = iNo ;
+    newn -> lchild = NULL ;
+    newn -> rchild = NULL ;
+
+    if(*first == NULL)
+    {
+        *first = newn ;         // root for every time insertion
+    }
+    else
+    {
+        temp = *first ;
+
+        while(1)
+        {
+            if(iNo > temp->data)
+            {
+                if(temp->rchild ==  NULL)
+                {
+                    temp -> rchild = newn ;
+                    break;
+                }
+
+                temp = temp->rchild ;
+            }
+            else if(iNo < temp -> data)
+            {
+                if(temp->lchild == NULL)
+                {
+                    temp ->lchild = newn ;
+                    break;
+                }
+                temp = temp->lchild;
+
+            }
+            else if (iNo == temp ->data)
+            {
+                printf("Unable to Insert as element is duplicate\n");
+                free(newn);               
+                break;                   //
+            }
+
+
+        }
+    }
+}
+
+
+int main()
+{
+    PNODE head = NULL;
+
+    Insert(&head,11);    // root 
+    Insert(&head,5);
+    Insert(&head,17);
+    Insert(&head,5);
+
+
+
+
+
+    return 0 ;
+}
