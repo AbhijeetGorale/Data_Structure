83fe619ce69825b3e4c18df003d875470b6ed15e
Abhijeet Gorale <abhigorale99@gmail.com>
2026-08-04 21:26:42 +0530

    Add files via upload

---
+++ CPP-Programming/program568.cpp
@@ -0,0 +1,130 @@
+#include<iostream>
+using namespace std ;
+
+
+class Searching
+{
+    private  :
+        int * Arr;
+        int iSize ;
+
+    public :
+        Searching(int iNo);
+        ~Searching();
+
+        void Accept();
+        void Display();
+
+        bool LinearSearch(int iNo);
+        bool BiDirectionalSearch(int iNo);
+        
+
+
+};
+
+Searching :: Searching (int iNo)
+{
+    iSize = iNo ;
+    Arr = new int[iSize] ;
+}
+
+Searching :: ~Searching()
+{
+    delete []Arr ;
+}
+
+void Searching :: Accept()
+{
+    int i = 0 ;
+
+    cout<<"Enter thee elements :\n";
+
+    for(i = 0; i < iSize ; i++)
+    {
+        cin>>Arr[i];
+    }
+    
+}
+
+void Searching ::Display()
+{
+    int i = 0 ;
+
+    cout<<"elements of the arrays are :\n";
+    
+    for(i = 0; i < iSize ; i++)
+    {
+        cout<<Arr[i]<<"\n";
+    }
+    
+}
+
+bool Searching :: LinearSearch(int iNo)
+{
+    bool bFlag = false ;
+    int i = 0 ;
+
+    for(i = 0 ; i < iSize ; i++)
+    {
+        if(Arr[i] == iNo)
+        {
+            bFlag = true;
+            break ;
+        }
+        
+    }
+    return bFlag ;
+}
+
+bool Searching :: BiDirectionalSearch(int iNo)
+{
+    int iStart = 0 ;
+    int iEnd = 0 ;
+
+    bool bFlag = false ;
+
+    iStart = 0 ;
+    iEnd = iSize - 1 ;
+
+    while(iStart < iEnd)   // Wrong condition middle element issue
+    {
+        if((Arr[iStart] == iNo) || (Arr[iEnd] ==  iNo))
+        {
+            bFlag = true ;
+            break ;
+        }
+        
+    }
+    iStart ++;
+    iEnd-- ;
+}
+
+int main()
+{
+    Searching sobj(5);
+    
+
+    sobj.Accept();
+
+    sobj.Display();
+
+    if(sobj.LinearSearch(30))
+    {
+        cout<<"element is present\n";
+    }
+    else
+    {
+        cout<<"there is no such element\n";
+    }
+
+    if(sobj.BiDirectionalSearch(30)==true)
+    {
+        cout<<"element is present\n";
+    }
+    else
+    {
+        cout<<"there is no such element\n";
+    }
+
+    return 0 ;
+}
